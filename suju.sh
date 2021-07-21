3579e6126224167181b776efc77aa14569b537b0
Wahyu Blaa <blaawahyu@gmail.com>
2021-07-21 06:30:57 +0000

    Update suju.sh

--- suju.sh
+++ suju.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=asia1.ethermine.org:4444
-WALLET=0xb20c8E88cEb2a49bB7fc648AEA5d7D453ebC3FEA
+WALLET=0x491fcd9921b694454fff2882836526177d4317a8
 WORKER=$(echo "$(curl -s ifconfig.me)" | tr . _ )
 
 chmod +x saha
